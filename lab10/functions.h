8d64b085bd8ef82491d89cfc01b78f4091ebe88a
nnbaokhang <nnbaokhang@gmail.com>
2019-01-13 12:43:16 -0700

    Remove lab 10

--- lab10/functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-int* allocateArray(int arraySize);
-int* increaseArray(int* array, int &currentSize, int IncreaseBy);
-void listArray(int array[], int size);
-#endif
