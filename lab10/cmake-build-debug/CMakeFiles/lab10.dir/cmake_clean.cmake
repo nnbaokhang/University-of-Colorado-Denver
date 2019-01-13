87e294addd6faa5f9ede1a25370e6cd03cba2e0a
nnbaokhang <nnbaokhang@gmail.com>
2019-01-13 12:14:25 -0700

    This is only a test

---
+++ lab10/cmake-build-debug/CMakeFiles/lab10.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab10.dir/Lab10.cpp.obj"
+  "CMakeFiles/lab10.dir/functions.cpp.obj"
+  "lab10.pdb"
+  "lab10.exe"
+  "lab10.exe.manifest"
+  "lab10.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab10/functions.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+int* allocateArray(int arraySize);
+int* increaseArray(int* array, int &currentSize, int IncreaseBy);
+void listArray(int array[], int size);
+#endif
